59e476e8aea69d37443dfd84ef40f130dcc1bd76
Ryan O <ryanou@Ryans-MacBook-Air.local>
2026-05-21 15:00:52 +1000

    Add blog writer agent + expand to 12 competitors

--- agent/competitors.js
+++ agent/competitors.js
@@ -37,4 +37,46 @@ export const COMPETITORS = [
     sitemap: "https://www.beginbright.com.au/sitemap.xml",
     homepage: "https://www.beginbright.com.au",
   },
+  {
+    slug: "learnmate",
+    name: "Learnmate",
+    sitemap: "https://learnmate.com.au/sitemap.xml",
+    homepage: "https://learnmate.com.au",
+  },
+  {
+    slug: "apex-tuition",
+    name: "Apex Tuition Australia",
+    sitemap: "https://www.apextuitionaustralia.com/sitemap.xml",
+    homepage: "https://www.apextuitionaustralia.com",
+  },
+  {
+    slug: "alchemy-tuition",
+    name: "Alchemy Tuition",
+    sitemap: "https://alchemytuition.com.au/sitemap.xml",
+    homepage: "https://alchemytuition.com.au",
+  },
+  {
+    slug: "success-tutoring",
+    name: "Success Tutoring",
+    sitemap: "https://successtutoring.com.au/sitemap.xml",
+    homepage: "https://successtutoring.com.au",
+  },
+  {
+    slug: "north-shore",
+    name: "North Shore Coaching College",
+    sitemap: "https://www.north-shore.com.au/sitemap.xml",
+    homepage: "https://www.north-shore.com.au",
+  },
+  {
+    slug: "tutoring-for-excellence",
+    name: "Tutoring for Excellence",
+    sitemap: "https://www.tutoringforexcellence.com.au/sitemap.xml",
+    homepage: "https://www.tutoringforexcellence.com.au",
+  },
+  {
+    slug: "math-minds",
+    name: "Math Minds",
+    sitemap: "https://mathminds.com.au/sitemap.xml",
+    homepage: "https://mathminds.com.au",
+  },
 ];
